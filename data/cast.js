6672ad1d22c7d41416b003bdcad9c11ff90bc24a
Edd Kleszcz <edd@edds-mbp.home>
2021-12-16 17:07:18 +0000

    Adding data into files

---
+++ data/cast.js
@@ -0,0 +1,9 @@
+module.exports = [
+    {
+        first_name: "Sigourney",
+        last_name: "Weaver",
+        dob: new Date(1949,9,8),
+        number_of_awards: 32,
+        number_of_nominations: 56        
+    }
+]
---
+++ data/movies.js
@@ -0,0 +1,410 @@
+module.exports = [{
+    "title": "Alien",
+    "genre": "Horror",
+    "year": 1979,
+    "director": "Ridley Scott",
+    "cast": ["Sigourney Weaver", "Tom Skerritt", "Veronica Cartwright", "Harry Dean Stanton", "John Hurt", "Ian Holm", "Yaphet Kotto"],
+    "ratings": {
+        "iMDB": 8.04,
+        "rottenTomatoes": [98, 94]
+    }
+},
+{
+    "title": "Aliens",
+    "genre": "Horror",
+    "year": 1986,
+    "director": "James Cameron",
+    "cast": ["Sigourney Weaver", "Bill Paxton", "Michael Biehn", "Carrie Henn", "Lance Henriksen", "Paul Reiser", "Jenette Goldstein"],
+    "ratings": {
+        "iMDB": 8.3,
+        "rottenTomatoes": [97, 94]
+    }
+},
+{
+    "title": "Alien 3",
+    "genre": "Horror",
+    "year": 1992,
+    "director": "David Fincher",
+    "cast": ["Sigourney Weaver", "Charles Dillon", "Charles Dance", "Brian Glover", "Ralph Brown", "Danny Webb", "Lance Henriksen"],
+    "ratings": {
+        "iMDB": 6.5,
+        "rottenTomatoes": [45, 47]
+    }
+},
+{
+   "title": "Ace Ventura: Pet Detective",
+   "genre": "Comedy",
+   "year": 1994,
+   "director": "Tom Shadyac",
+   "cast": ["Jim Carrey", "Sean Young", "Courteney Cox", "Tone Loc", "Dan Marino"],
+   "ratings": {
+       "iMDB": 6.9,
+       "rottenTomatoes": [49, 57]
+   }
+},
+{
+    "title": "Back to the Future",
+    "genre": "Science Fiction",
+    "year": 1985,
+    "director": "Robert Zemeckis",
+    "cast": ["Michael Fox", "Christopher Lloyd", "Lea Thompson", "Crispin Glover"],
+    "ratings": {
+        "iMDB": 8.5,
+        "rottenTomatoes": [96, 94]
+    }
+},
+{
+    "title": "Akira",
+    "genre": "Anime",
+    "year": 1988,
+    "director": "Katsuhiro Otomo",
+    "cast": ["Mitsuo Iwata", "Nozomu Sasaki", "Mami Koyama", "Taro Ishida", "Mizuho Suzuki", "Tetsusho Genada"],
+    "ratings": {
+        "iMDB": 8.0,
+        "rottenTomatoes": [90, 90]
+    }
+},
+{
+    "title": "Star Wars: Empire Strikes Back",
+    "genre": "Science Fiction",
+    "year": 1980,
+    "director": "Irvin Kershner",
+    "cast": ["Mark Hamill", "Carrie Fisher", "Bruno Kirby", "Harrison Ford", "Billy Dee Williams", "Anthony Daniels", "David Prowse", "Kenny Baker", "Peter Mayhew", "Frank Oz"],
+    "ratings": {
+        "iMDB": 8.7,
+        "rottenTomatoes": [94, 97]
+    }
+},
+{
+    "title": "RoboCop",
+    "genre": "Science Fiction",
+    "year": 1987,
+    "director": "Paul Verhoeven",
+    "cast": ["Peter Weeler", "Nancy Allen", "Daniel O'Herlihy", "Ronny Cox", "Kurtwood Smith", "Miguel Ferrer"],
+    "ratings": {
+        "iMDB": 7.6,
+        "rottenTomatoes": [90, 84]
+    }
+},
+{
+    "title": "Hellraiser",
+    "genre": "Horror",
+    "year": 1987,
+    "director": "Clive Barker",
+    "cast": ["Andrew Robinson", "Clare Higgins", "Ashley Laurence"],
+    "ratings":{
+        "iMDB": 7.0,
+        "rottenTomatoes": [72, 72]
+    }
+},
+{
+    "title": "Ghostbusters",
+    "genre": "Science Fiction",
+    "year": 1984,
+    "director": "Ivan Reitman",
+    "cast": ["Bill Murray", "Dan Aykroyd", "Sigourney Weaver", "Harold Ramis", "Rick Moranis"],
+    "ratings": {
+        "iMDB": 7.8,
+        "rottenTomatoes": [74, 49]
+    }
+},
+{
+    "title": "Legally Blonde",
+    "genre": "Comedy",
+    "year": 2001,
+    "director": "Robert Luketic",
+    "cast": ["Reese Witherspoon", "Luke Wilson", "Selma Blair", "Matthew Davis", "Victor Garber", "Jennifer Coolidge"],
+    "ratings": {
+        "iMDB": 6.4,
+        "rottenTomatoes": [70, 72]
+    }
+},
+{
+    "title": "When Harry Met Sally",
+    "genre": "Romantic Comedy",
+    "year": 1989,
+    "director": "Rob Reiner",
+    "cast": ["Billy Crystal", "Meg Ryan", "Carrie Fisher", "Bruno Kirby"],
+    "ratings":{
+        "iMDB": 7.6,
+        "rottenTomatoes": [91, 89]
+    }
+},
+{
+    "title": "Scott Pilgrim vs the World",
+    "genre": "Romantic Comedy",
+    "year": 2010,
+    "director": "Edgar Wright",
+    "cast": ["Michael Cera", "Mary Elizabeth Winstead", "Kieran Culkin", "Chris Evans", "Anna Kendrick", "Alison Pill", "Brandon Routh", "Jason Schwartzman"],
+    "ratings": {
+        "iMDB": 7.5,
+        "rottenTomatoes": [82, 84]
+    }
+},
+{
+    "title": "Nomadland",
+    "genre": "Drama",
+    "year": 2020,
+    "director": "Chole Zhao",
+    "cast": ["Frances McDormand", "David Strathairn", "Linda May", "Swankie"],
+    "ratings": {
+        "iMDB": 7.4,
+        "rottenTomatoes": [93, 82]
+    }
+},
+{
+    "title": "No Time To Die",
+    "genre": "Action",
+    "year": 2021,
+    "director": "Cary Joji Fukunaga",
+    "cast": ["Daniel Craig", "Rami Malek", "Lea Seydoux", "Lashana Lynch", "Ben Whishaw", "Naomie Harris", "Jeffrey Wright", "Christoph Waltz", "Ralph Fiennes"],
+    "ratings": {
+        "iMDB": 7.5,
+        "rottenTomatoes": [84, 88]
+    }
+},
+{
+    "title": "Pulp Fiction",
+    "genre": "Comedy",
+    "year": 1994,
+    "director": "Quentin Tarantino",
+    "cast": ["John Travolta", "Samuel L. Jackson", "Uma Thurman", "Harvey Keitel", "Tim Roth", "Amanda Plummer", "Maria de Medeiros", "Ving Rhames", "Eric Stoltz", "Rosanna Arquette", "Christopher Walken", "Bruce Willis"],
+    "ratings": {
+        "iMDB": 8.9,
+        "rottenTomatoes": [92, 96]
+    }
+},
+{
+    "title": "Predator",
+    "genre": "Science Fiction",
+    "year": 1987,
+    "director": "John Mctiernan",
+    "cast": ["Arnold Schwarzeneggar", "Carl Weathers", "Elpidia Carrillo", "Bill Duke", "Richard Chaves"],
+    "ratings": {
+        "iMDB": 7.8,
+        "rottenTomatoes": [82, 87]
+    }
+},
+{
+    "title": "Raiders of the Lost Ark",
+    "genre": "Action",
+    "year": 1981,
+    "director": "Steven Spielberg",
+    "cast": ["Harrison Ford", "Karen Allen", "Paul Freeman", "Ronald Lacey", "John Rhys-Davies", "Denholm Elliot"],
+    "ratings": {
+        "iMDB": 8.4,
+        "rottenTomatoes": [95, 96]
+    }
+},
+{
+    "title": "The Shining",
+    "genre": "Horror",
+    "year": 1980,
+    "director": "Stanley Kubrick",
+    "cast": ["Jack Nicholson", "Shelly Duvall", "Scatman Crothers", "Danny Lloyd"],
+    "ratings": {
+        "iMDB": 8.4,
+        "rottenTomatoes": [84, 93]
+    }
+},
+{
+    "title": "The Exorcist",
+    "genre": "Horror",
+    "year": 1973,
+    "director": "William Friedkin",
+    "cast": ["Ellen Burstyn", "Max von Sydow", "Lee Cobb", "Kitty Winn", "Jack MacGowran", "Jason Miller", "Linda Blair"],
+    "ratings": {
+        "iMDB": 8.0,
+        "rottenTomatoes": [83, 87]
+    }
+},
+{
+    "title": "500 Days of Summer",
+    "genre": "Romantic Comedy",
+    "year": 2009,
+    "director": "Marc Webb",
+    "cast": ["Joseph Gordon-Levitt", "Zooey Deschanel"],
+    "ratings": {
+        "iMDB": 7.7,
+        "rottenTomatoes": [85, 84]
+    }
+},
+{
+    "title": "Harry Potter and the Philosopher's Stone",
+    "genre": "Fantasy",
+    "year": 2001,
+    "director": "Chris Columbus",
+    "cast": ["Daniel Radcliffe", "Rupert Grint", "Emma Watson", "John Cleese", "Robbie Coltrane", "Warwick Davis", "Richard Griffiths", "Richard Harris", "Ian Hart", "John Hurt", "Alan Rickman", "Fiona Shaw", "Maggie Smith", "Julie Walters"],
+    "ratings": {
+        "iMDB": 7.6,
+        "rottenTomatoes": [81, 82]
+    }
+},
+{
+    "title": "Harry Potter and the Chamber of Secrets",
+    "genre": "Fantasy",
+    "year": 2002,
+    "director": "Chris Columbus",
+    "cast": ["Daniel Radcliffe", "Rupert Grint", "Emma Watson", "Kenneth Branagh", "John Cleese", "Robbie Coltrane", "Warwick Davis", "Richard Griffiths", "Richard Harris", "Jason Isaacs", "Alan Rickman", "Fiona Shaw", "Maggie Smith", "Julie Walters"],
+    "ratings": {
+        "iMDB": 7.4,
+        "rottenTomatoes": [82, 80]
+    }
+},
+{
+    "title": "Harry Potter and the Prisoner of Azkaban",
+    "genre": "Fantasy",
+    "year": 2004,
+    "director": "Alfonso Cuaron",
+    "cast": ["Daniel Radcliffe", "Rupert Grint", "Emma Watson", "Robbie Coltrane", "Michael Gambon", "Richard Griffiths", "Gary Oldman", "Timothy Spall", "David Thewlis", "Emma Thompson", "Alan Rickman", "Fiona Shaw", "Maggie Smith", "Julie Walters"],
+    "ratings": {
+        "iMDB": 7.9,
+        "rottenTomatoes": [90, 86]
+    }
+},
+{
+    "title": "Harry Potter and the Goblet of Fire",
+    "genre": "Fantasy",
+    "year": 2005,
+    "director": "Mike Newell",
+    "cast": ["Daniel Radcliffe", "Rupert Grint", "Emma Watson", "Robbie Coltrane", "Ralph Fiennes", "Michael Gambon", "Brendan Gleeson", "Jason Isaacs", "Gary Oldman", "Robert Pattinson", "Clemence Poesy", "Stanislav Ianevski", "Timothy Spall", "Alan Rickman", "Maggie Smith"],
+    "ratings": {
+        "iMDB": 7.7,
+        "rottenTomatoes": [88, 74]
+    }
+},
+{
+    "title": "Harry Potter and the Order of the Phoenix",
+    "genre": "Fantasy",
+    "year": 2007,
+    "director": "David Yates",
+    "cast": ["Daniel Radcliffe", "Rupert Grint", "Emma Watson", "Robbie Coltrane", "Helena Bonham Carter", "Warwick Davis", "Ralph Fiennes", "Michael Gambon", "Richard Griffiths", "Jason Isaacs", "Fiona Shaw", "Imelda Staunton", "David Thewlis", "Emma Thompson", "Julie Walters", "Brendan Gleeson", "Jason Isaacs", "Gary Oldman", "Alan Rickman", "Maggie Smith"],
+    "ratings": {
+        "iMDB": 7.5,
+        "rottenTomatoes": [77, 81]
+    }
+},
+{
+    "title": "Harry Potter and the Half Blood Prince",
+    "genre": "Fantasy",
+    "year": 2009,
+    "director": "David Yates",
+    "cast": ["Daniel Radcliffe", "Rupert Grint", "Emma Watson", "Robbie Coltrane", "Helena Bonham Carter", "Jim Broadbent", "Warwick Davis", "Timothy Spall", "Michael Gambon",  "David Thewlis", "Julie Walters", "Alan Rickman", "Maggie Smith"],
+    "ratings": {
+        "iMDB": 7.6,
+        "rottenTomatoes": [84, 78]
+    }
+},
+{
+    "title": "Harry Potter and the Deathly Hallows Part 1",
+    "genre": "Fantasy",
+    "year": 2010,
+    "director": "David Yates",
+    "cast": ["Daniel Radcliffe", "Rupert Grint", "Emma Watson", "Robbie Coltrane", "Helena Bonham Carter", "John Hurt", "Ralph Fiennes", "Brendan Gleeson", "Warwick Davis", "Jason Isaacs", "Richard Griffiths", "Fiona Shaw", "Imelda Staunton", "Timothy Spall", "Michael Gambon",  "David Thewlis", "Julie Walters", "Alan Rickman", "Maggie Smith"],
+    "ratings": {
+        "iMDB": 7.7,
+        "rottenTomatoes": [77, 85]
+    }
+},
+{
+    "title": "Harry Potter and the Deathly Hallows Part 2",
+    "genre": "Fantasy",
+    "year": 2011,
+    "director": "David Yates",
+    "cast": ["Daniel Radcliffe", "Rupert Grint", "Emma Watson", "Robbie Coltrane", "Helena Bonham Carter", "John Hurt", "Ralph Fiennes", "Warwick Davis", "Jason Isaacs", "Michael Gambon",  "David Thewlis", "Julie Walters", "Alan Rickman", "Maggie Smith"],
+    "ratings": {
+        "iMDB": 8.1,
+        "rottenTomatoes": [96, 89]
+    }
+},
+{
+    "title": "Star Wars: Return of the Jedi",
+    "genre": "Science Fiction",
+    "year": 1983,
+    "director": "Richard Marquand",
+    "cast": ["Mark Hamill", "Harrison Ford", "Carriew Fisher", "Billy Dee Williams", "Anthony Daniels", "David Prowse", "Kenny Baker", "Peter Mayhew", "Frank Oz"],
+    "ratings": {
+        "iMDB": 8.3,
+        "rottenTomatoes": [82, 94]
+    }
+},
+{
+    "title": "Star Wars: A New Hope",
+    "genre": "Science Fiction",
+    "year": 1977,
+    "director": "George Lucas",
+    "cast": ["Mark Hamill", "Harrison Ford", "Carrie Fisher", "Peter Cushing", "Alec Guiness"],
+    "ratings": {
+        "iMDB": 8.6,
+        "rottenTomatoes": [92, 96]
+    }
+},
+{
+    "title": "Blade Runner",
+    "genre": "Science Fiction",
+    "year": 1982,
+    "director": "Ridley Scott",
+    "cast": ["Harrison Ford", "Rutger Hauer", "Sean Young", "Edward James Olmos"],
+    "ratings": {
+        "iMDB": 8.1,
+        "rottenTomatoes": [89, 91]
+    }
+},
+{
+    "title": "Detective Pikachu",
+    "genre": "Drama",
+    "year": 2019,
+    "director": "Rob Letterman",
+    "cast": ["Ryan Reynolds", "Justice Smith", "Kathryn Newton", "Suki Waterhouse", "Omar Chaparro", "Chris Geere", "Ken Watanabe", "Bill Nighy"],
+    "ratings": {
+        "iMDB": 6.6,
+        "rottenTomatoes": [68, 79]
+    }
+},
+{
+    "title": "Pokemon: The First Movie",
+    "genre": "Anime",
+    "year": 1999,
+    "director": "Kunihiko Yuyama",
+    "cast": ["Veronica Taylor", "Rachael Lillis", "Eric Stuart"],
+    "ratings": {
+        "iMDB": 6.3,
+        "rottenTomatoes": [16, 63]
+    }
+},
+{
+    "title": "Spirited Away",
+    "genre": "Anime",
+    "year": 2001,
+    "director": "Hayao Miyazaki",
+    "cast": ["Rumi Hiiragi", "Miyu Irino", "Mari Natsuki", "Takashi Naito", "Yasuki Sawaguchi", "Tatsuya Gashuin"],
+    "ratings": {
+        "iMDB": 8.6,
+        "rottenTomatoes": [97, 96]
+    }
+},
+{
+    "title": "The Curious Case of Benjamin Button",
+    "genre": "Romance",
+    "year": 2008,
+    "director": "David Fincher",
+    "cast": ["Brad Pitt", "Cate Blanchett", "Tilda Swindon", "Julia Ormond"],
+    "ratings": {
+        "iMDB": 7.8,
+        "rottenTomatoes": [71, 80]
+    }
+},
+
+]
+
+// {
+//     "title":
+//     "genre": 
+//     "year": 
+//     "director": 
+//     "cast": 
+//     "ratings": {
+//         "iMDB":
+//         "rottenTomatoes": []
+//     }
+// },
