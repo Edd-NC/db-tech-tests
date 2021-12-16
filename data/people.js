f5037498a1c437c8d4561a35ef3a3324be036116
Edd Kleszcz <edd@edds-mbp.home>
2021-12-16 13:10:50 +0000

    Adding data of people

---
+++ data/people.js
@@ -0,0 +1,605 @@
+module.exports = [
+    {
+        first_name: "Edd",
+        last_name: "Cole",
+        dob: new Date(1992, 10, 13),
+        likes: ["Horror", "Science Fiction", "Fantasy", "Action"],
+        dislikes: ["Romance", "Romantic Comedy"],
+        eats: [
+            "Strawberry Ice Cream", "Haribo Tangfastics", "Salt & Vinegar Crisps", "Lasagne", "Pizza", "Calzone", "Mango", "Panna Cotta"],
+        favourite_film: "Alien"
+    },
+    {
+        first_name: "Stephen",
+        last_name: "Corke",
+        dob: new Date(1977, 4, 19),
+        likes: ["Romance", "Science Fiction", "Fantasy"],
+        dislikes: ["Horror", "Anime"],
+        eats: ["Pizza", "Salad", "Fruit Salad", "Haribo Starmix", "Prawn Cocktail Crisps", "Lasagne", "Calzone", "Banana", "Broccoli"],
+        favourite_film: "Back to the Future"
+    },
+    {
+        first_name: "Paul",
+        last_name: "Copley",
+        dob: new Date(1985, 9, 6),
+        likes: ["Science Fiction", "Fantasy", "Anime", "Comedy", "Horro"],
+        dislikes: ["Romance"],
+        eats: [
+            'Haribo Supermix',
+            'Strawberry Ice Cream',
+            'Prawn Cocktail Crisps',
+            'Orange',
+            'Chocolate Ice Cream',
+            'Leek',
+            'Chocolate Cake',
+            'Ratatouille'
+          ],
+        favourite_film: "Star Wars: Empire Strikes Back"
+    },
+    {
+        first_name: "Harry",
+        last_name: "Smith",
+        dob: new Date(1994, 10, 2),
+        likes: ["Horror", "Comedy"],
+        dislikes: ["Science Fiction", "Anime", "Fantasy", "Romantic Comedy"],
+        eats: [
+            'Chocolate Cake',
+            'Lasagne',
+            'Fruit Salad',
+            'Panna Cotta',
+            'Banana',
+            'Deep Fried Mars Bar',
+            'Skittles',
+            'Leek',
+            'Salted Caramel Ice Cream',
+            'Mango'
+          ],
+        favourite_film: "The Exorcist"
+    },
+    {
+        first_name: "John",
+        last_name: "Smith",
+        dob: new Date(1994, 10, 2),
+        likes: ["Fantasy", "Science Fiction"],
+        dislikes: ["Comedy", "Horror"],
+        eats: [
+            'Broccoli',
+            'Haribo Tangfastics',
+            'Brussel Sprouts',
+            'Skittles',
+            'Pizza',
+            'Lasagne',
+            'Ratatouille',
+            'Calzone',
+            'Chocolate Cake',
+            'Scotch Egg'
+          ],
+        favourite_film: "Harry Potter and the Prisoner of Azkaban"
+    },
+    {
+        first_name: "Sarah",
+        last_name: "Baines",
+        dob: new Date(1988, 2, 10),
+        likes: ["Romance", "Fantasy", "Science Fiction", "Comedy", "Romantic Comedy"],
+        dislikes: ["Horror", "Action"],
+        eats: [
+            'Mars Bar',
+            'Deep Fried Mars Bar',
+            'Chocolate Ice Cream',
+            'Scotch Egg',
+            'Ham Sandwich',
+            'Prawn Cocktail Crisps',
+            'Haribo Starmix',
+            'Sausage Roll',
+            'Broccoli'
+          ],
+        favourite_film: "Pulp Fiction"
+    },
+    {
+        first_name: "Vel",
+        last_name: "Georgiva",
+        dob: new Date(1987, 7,7),
+        likes: ["Action", "Romance", "Romantic Comedy", "Comedy"],
+        dislikes: ["Fantasy", "Science Fiction"],
+        eats: [
+            'Strawberry Ice Cream',
+            'Cheese Sandwich',
+            'Apple',
+            'Scotch Egg',
+            'Fruit Salad',
+            'Tiramisu',
+            'Broccoli',
+            'Deep Fried Mars Bar',
+            'Chocolate'
+        ],
+        favourite_film: "500 Days of Summer"
+    },
+    {
+        first_name: "Jo",
+        last_name: "Imlay",
+        dob: new Date(1985, 6, 24),
+        likes: ["Anime", "Fantasy", "Comedy", "Horror", "Science Fiction"],
+        dislikes: ["Action"],
+        eats: [
+            'Celery',
+            'Coconut',
+            'Chocolate Ice Cream',
+            'Pizza',
+            'Apple',
+            'Tiramisu',
+            'Brussel Sprouts',
+            'Panna Cotta',
+            'Sausage Roll',
+            'Pork Pie'
+          ],
+        favourite_film: "Akira"
+    },
+    {
+        first_name: "Duncan",
+        last_name: "Crawley",
+        dob: new Date(1991, 9, 31),
+        likes: ["Comedy", "Fantasy"],
+        dislikes: ["Action", "Anime", "Science Fiction"],
+        eats: [
+            'Calzone',
+            'Pot Noodle',
+            'Cheese Sandwich',
+            'Chocolate Cake',
+            'Ham Sandwich',
+            'Roast Dinner',
+            'Coconut',
+            'Grapes'
+          ],
+        favourite_film: "Ace Ventura: Pet Detective"
+    },
+    {
+        first_name: "Chipie",
+        last_name: "Monk",
+        dob: new Date(1990, 11, 25),
+        likes: ["Action", "Drama", "Science Fiction"],
+        dislikes: ["Horror", "Romance", "Romantic Comedy"],
+        eats: [
+            'Fish & Chips',
+            'Calzone',
+            'Strawberry Ice Cream',
+            'Crackers',
+            'Orange',
+            'Pork Pie',
+            'Deep Fried Mars Bar',
+            'Prawn Cocktail Crisps',
+            'Lasagne',
+            'Mars Bar'
+          ],
+        favourite_film: "Nomadland"
+    },
+    {
+        first_name: "Jonny",
+        last_name: "Rathbone",
+        dob: new Date(1980, 0, 6),
+        likes: ["Action"],
+        dislikes: ["Comedy", "Fantasy", "Horror"],
+        eats: [
+            'Apple',
+            'Coconut',
+            'Roast Dinner',
+            'Leek',
+            'Mango',
+            'Mars Bar',
+            'Brussel Sprouts',
+            'Orange',
+            'Lasagne'
+          ],
+        favourite_film: "No Time To Die"
+    },
+    {
+        first_name: "Sam",
+        last_name: "P",
+        dob: new Date(1986, 04, 12),
+        likes: ["Fantasy", "Science Fiction"],
+        dislikes: ["Romantic Comedy", "Action"],
+        eats: [
+            'Pizza',
+            'Sausage Roll',
+            'Fish & Chips',
+            'Orange',
+            'Leek',
+            'Crackers',
+            'Deep Fried Mars Bar',
+            'Coconut',
+            'Grapes',
+            'Haribo Tangfastics'
+          ],
+        favourite_film: "Harry Potter and the Deathly Hallows Part 2"
+    },
+    {
+        first_name: "Doctor",
+        last_name: "Who",
+        dob: new Date(1970, 1, 27),
+        likes: ["Science Fiction", "Romance", "Romantic Comedy", "Fantasy"],
+        dislikes: ["Action", "Drama"],
+        eats: [
+            'Pot Noodle',
+            'Sausage Roll',
+            'Mars Bar',
+            'Strawberry Ice Cream',
+            'Carrot',
+            'Calzone',
+            'Haribo Supermix',
+            'Fish & Chips',
+            'Passionfruit',
+            'Coconut'
+          ],
+        favourite_film: ["When Harry Met Sally"]
+    },
+    {
+        first_name: "Harry",
+        last_name: "Potter",
+        dob: new Date(1980, 6, 31),
+        likes: ["Fantasy", "Action", "Drama"],
+        dislikes: ["Science Fiction", "Comedy", "Romantic Comedy"],
+        eats: [
+            'Chocolate',
+            'Apple',
+            'Orange',
+            'Mango',
+            'Roast Dinner',
+            'Scotch Egg',
+            'Cheese Sandwich',
+            'Sausage Roll',
+            'Chocolate Ice Cream'
+          ],
+        favourite_film: "Harry Potter and the Philosophers Stone"
+    },
+    {
+        first_name: "Ronald",
+        last_name: "Weasley",
+        dob: new Date(1980, 2, 1),
+        likes: ["Action", "Comedy", "Fantasy", "Science Fiction"],
+        dislikes: ["Romance", "Romantic Comedy", "Drama"],
+        eats: [
+            'Haribo Tangfastics',
+            'Haribo Starmix',
+            'Pot Noodle',
+            'Scotch Egg',
+            'Salt & Vinegar Crisps',
+            'Sausage Roll',
+            'Pork Pie',
+            'Grapes',
+            'Jelly Beans',
+            'Skittles'
+          ],
+        favourite_film: "Harry Potter and the Half Blood Prince"
+    },
+    {
+        first_name: "Hermione",
+        last_name: "Granger",
+        dob: new Date(1979, 8, 19),
+        likes: ["Drama", "Fantasy", "Science Fiction", "Comedy"],
+        dislikes: ["Horror", "Action"],
+        eats: [
+            'Chocolate Cake',
+            'Salad',
+            'Cheese & Onion Crisps',
+            'Celery',
+            'Leek',
+            'Fruit Salad',
+            'Apple',
+            'Orange',
+            'Brussel Sprouts'
+          ],
+        favourite_film: "Harry Potter and the Deathly Hallows Part 2"
+    },
+    {
+        first_name: "Draco",
+        last_name: "Malfoy",
+        dob: new Date(1980, 5, 5),
+        likes: ["Action", "Horror"],
+        dislikes: ["Romance", "Romantic Comedy", "Comedy"],
+        eats: [
+            'Skittles',
+            'Leek',
+            'Ham Sandwich',
+            'Orange',
+            'Calzone',
+            'Carrot',
+            'Banana',
+            'Pork Pie',
+            'Salt & Vinegar Crisps'
+          ],
+        favourite_film: "Harry Potter and the Chamber of Secrets"
+    },
+    {
+        first_name: "Ellen",
+        last_name: "Ripley",
+        dob: new Date(1952, 11, 17),
+        likes: ["Action", "Science Fiction"],
+        dislikes: ["Romance", "Romantic Comedy"],
+        eats: [
+            'Roast Dinner',
+            'Banana',
+            'Cheese Sandwich',
+            'Pizza',
+            'Carrot',
+            'Coconut',
+            'Grapes',
+            'Passionfruit'
+          ],
+        favourite_film: "Aliens"
+    },
+    {
+        first_name: "Jatinder",
+        last_name: "Singh",
+        dob: new Date(1982, 5,8),
+        likes: ["Action", "Science Fiction", "Fantasy"],
+        dislikes: ["Drama", "Horror"],
+        eats: [
+            'Sausage Roll',
+            'Prawn Cocktail Crisps',
+            'Broccoli',
+            'Panna Cotta',
+            'Pot Noodle',
+            'Apple',
+            'Ham Sandwich',
+            'Jelly Beans',
+            'Chocolate Cake',
+            'Grapes'
+          ],
+        favourite_film: "Ghostbusters"
+    },
+    {
+        first_name: "Luke",
+        last_name: "Skywalker",
+        dob: new Date(1981, 11, 6),
+        likes: ["Action", "Science Fiction", "Romance"],
+        dislikes: ["Horror", "Drama"],
+        eats: [
+            'Ratatouille',
+            'Chocolate Ice Cream',
+            'Celery',
+            'Sausage Roll',
+            'Chocolate Cake',
+            'Panna Cotta',
+            'Orange',
+            'Ham Sandwich'
+          ],
+        favourite_film: "Star Wars: Return of the Jedi"
+    },
+    {
+        first_name: "Leia",
+        last_name: "Organa",
+        dob: new Date(1981, 11, 6),
+        likes: ["Comedy", "Drama", "Science Fiction"],
+        dislikes: ["Horror", "Action"],
+        eats: [
+            'Leek',
+            'Calzone',
+            'Chocolate Ice Cream',
+            'Tiramisu',
+            'Banana',
+            'Ham Sandwich',
+            'Haribo Tangfastics',
+            'Coconut',
+            'Salted Caramel Ice Cream'
+          ],
+        favourite_film: "Blade Runner"
+    },
+    {
+        first_name: "George",
+        last_name: "Weasley",
+        dob: new Date(1978, 9, 7),
+        likes: ["Comedy", "Action", "Romantic Comedy"],
+        dislikes: ["Drama", "Science Fiction"],
+        eats: [
+            'Coconut',
+            'Haribo Supermix',
+            'Orange',
+            'Haribo Tangfastics',
+            'Passionfruit',
+            'Prawn Cocktail Crisps',
+            'Cheese Sandwich',
+            'Haribo Starmix',
+            'Sausage Roll',
+            'Panna Cotta'
+          ],
+        favourite_film: "Harry Potter and the Order of the Phoenix"
+    },
+    {
+        first_name: "Fred",
+        last_name: "Weasley",
+        dob: new Date(1978, 9, 7),
+        likes: ["Comedy", "Action", "Romantic Comedy"],
+        dislikes: ["Drama", "Science Fiction"],
+        eats: [
+            'Coconut',
+            'Haribo Supermix',
+            'Orange',
+            'Haribo Tangfastics',
+            'Passionfruit',
+            'Prawn Cocktail Crisps',
+            'Cheese Sandwich',
+            'Haribo Starmix',
+            'Sausage Roll',
+            'Panna Cotta'
+          ],
+        favourite_film: "Harry Potter and the Order of the Phoenix"
+    },
+    {
+        first_name: "Ginny",
+        last_name: "Weasley",
+        dob: new Date(1981, 4, 6),
+        likes: ["Action", "Drama", "Comedy", "Science Fiction"],
+        dislikes: ["Romance", "Romantic Comedy"],
+        eats: [
+            'Pizza',
+            'Lasagne',
+            'Coconut',
+            'Roast Dinner',
+            'Sausage Roll',
+            'Haribo Starmix',
+            'Celery',
+            'Deep Fried Mars Bar',
+            'Mango',
+            'Panna Cotta'
+          ],
+        favourite_film: "Harry Potter and the Half Blood Prince"
+    },
+    {
+        first_name: "Albus",
+        last_name: "Dumbledore",
+        dob: new Date(1880, 7, 27),
+        likes: ["Romance", "Romantic Comedy", "Drama"],
+        dislikes: ["Horror", "Action"],
+        eats: [
+            'Prawn Cocktail Crisps',
+            'Fish & Chips',
+            'Coconut',
+            'Pork Pie',
+            'Mango',
+            'Salt & Vinegar Crisps',
+            'Ratatouille',
+            'Carrot',
+            'Gobstoppers'
+          ],
+        favourite_film: "Scott Pilgrim vs the World"
+    },
+    {
+        first_name: "Han",
+        last_name: "Solo",
+        dob: new Date(1977, 0, 14),
+        likes: ["Action", "Science Fiction", "Horror"],
+        dislikes: ["Fantasy", "Drama", "Romance", "Romantic Comedy"],
+        eats: [
+            'Ratatouille',
+            'Mango',
+            'Broccoli',
+            'Roast Dinner',
+            'Leek',
+            'Skittles',
+            'Salted Caramel Ice Cream',
+            'Pot Noodle',
+            'Salt & Vinegar Crisps'
+          ],
+        favourite_film: "Star Wars: A New Hope"
+    },
+    {
+        first_name: "Argus",
+        last_name: "Filch",
+        dob: new Date(1942, 3, 26),
+        likes: ["Horror", "Drama"],
+        dislikes: ["Comedy", "Romance", "Romantic Comedy"],
+        eats: [
+            'Carrot',
+            'Scotch Egg',
+            'Cheese & Onion Crisps',
+            'Salad',
+            'Brussel Sprouts',
+            'Leek',
+            'Crackers'
+          ],
+        favourite_film: "Harry Potter and the Order of the Phoenix"
+    },
+    {
+        first_name: "Patrick",
+        last_name: "Jones",
+        dob: new Date(1997, 5, 30),
+        likes: ["Action", "Romance", "Drama"],
+        dislikes: ["Science Fiction", "Comedy", "Fantasy"],
+        eats: [
+            'Jelly Beans',
+            'Haribo Supermix',
+            'Lasagne',
+            'Salt & Vinegar Crisps',
+            'Chocolate Cake',
+            'Grapes',
+            'Cheese & Onion Crisps',
+            'Calzone'
+          ],
+        favourite_film: "Hellraiser"
+    },
+    {
+        first_name: "Nicole",
+        last_name: "Talley",
+        dob: new Date(1995, 3, 19),
+        likes: ["Romance", "Romantic Comedy", "Comedy"],
+        dislikes: ["Action", "Horror", "Science Fiction"],
+        eats: [
+            'Strawberry Ice Cream',
+            'Jelly Beans',
+            'Sausage Roll',
+            'Broccoli',
+            'Calzone',
+            'Pizza',
+            'Mars Bar',
+            'Cheese & Onion Crisps',
+            'Apple',
+            'Haribo Supermix'
+          ],
+        favourite_film: "Legally Blonde"
+    },
+    {
+        first_name: "Alice",
+        last_name: "Banbury",
+        dob: new Date(1993, 1, 4),
+        likes: ["Action", "Science Fiction", "Fantasy", "Horror"],
+        dislikes: ["Romance", "Romantic Comedy"],
+        eats: [
+            'Fruit Salad',
+            'Apple',
+            'Leek',
+            'Chocolate Cake',
+            'Prawn Cocktail Crisps',
+            'Passionfruit',
+            'Grapes',
+            'Haribo Starmix',
+            'Gobstoppers',
+            'Roast Dinner'
+          ],
+        favourite_film: "Predator"
+    },
+    {
+        first_name: "Mike",
+        last_name: "Kentucky",
+        dob: new Date(1989, 3, 1),
+        likes: ["Action", "Comedy", "Drama"],
+        dislikes: ["Horror", "Anime"],
+        eats: [
+            'Mango',
+            'Passionfruit',
+            'Jelly Beans',
+            'Celery',
+            'Cheese Sandwich',
+            'Pot Noodle',
+            'Grapes',
+            'Cheese & Onion Crisps',
+            'Orange',
+            'Coconut'
+          ],
+        favourite_film: "No Time To Die"
+    },
+    {
+        first_name: "Will",
+        last_name: "Green",
+        dob: new Date(2007, 10, 26),
+        likes: ["Anime", "Comedy"],
+        dislikes: ["Horror", "Romance"],
+        eats: [
+            'Deep Fried Mars Bar',
+            'Chocolate Ice Cream',
+            'Haribo Tangfastics',
+            'Fish & Chips',
+            'Mars Bar',
+            'Gobstoppers',
+          ],
+        favourite_film: "Detective Pikachu"
+    }
+]
+
+// {
+//     first_name: ,
+//     last_name: ,
+//     dob: new Date(),
+//     likes:
+//     dislikes: 
+//     eats: 
+//     favourite_film:
+// },
