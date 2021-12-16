6d50f6f0810b705a6e960ba793e1db733a3832d2
Edd Kleszcz <edd@edds-mbp.home>
2021-12-16 13:11:19 +0000

    Adding snacks

---
+++ data/snacks.js
@@ -0,0 +1,278 @@
+module.exports = [
+    {
+        food: "Salt & Vinegar Crisps",
+        type: "Junk",
+        movie_friendly: false,
+        rating_out_of_5: 4.5 
+    },
+    {
+        food: "Haribo Starmix",
+        type: "Sweets",
+        movie_friendly: true,
+        rating_out_of_5: 3.5,
+    },
+    {
+        food: "Haribo Tangfastics",
+        type: "Sweets",
+        movie_friendly: true,
+        rating_out_of_5: 5
+    },
+    {
+        food: "Haribo Supermix",
+        type: "Sweets",
+        movie_friendly: true,
+        rating_out_of_5: 1
+    },
+    {
+        food: "Strawberry Ice Cream",
+        type: "Junk",
+        movie_friendly: true,
+        rating_out_of_5: 4
+    },
+    {
+        food: "Chocolate Ice Cream",
+        type: "Junk",
+        movie_friendly: true, 
+        rating_out_of_5: 4.5
+    },
+    {
+        food: "Salted Caramel Ice Cream",
+        type: "Junk",
+        movie_friendly: true,
+        rating_out_of_5: 4
+    },
+    {
+        food: "Prawn Cocktail Crisps",
+        type: "Junk",
+        movie_friendly: "false",
+        rating_out_of_5: 4.5,
+    },
+    {
+        food: "Lasagne",
+        type: "Meal",
+        movie_friendly: false,
+        rating_out_of_5: 5
+    },
+    {
+        food: "Ratatouille",
+        type: "Meal",
+        movie_friendly: false, 
+        rating_out_of_5: 3.5
+    },
+    {
+        food: "Cheese & Onion Crisps",
+        type: "Junk",
+        movie_friendly: false,
+        rating_out_of_5: 3
+    },
+    {
+        food: "Chocolate Cake",
+        type: "Dessert",
+        movie_friendly: true,
+        rating_out_of_5: 5
+    },
+    {
+        food: "Panna Cotta",
+        type: "Dessert",
+        movie_friendly: false,
+        rating_out_of_5: 5
+    },
+    {
+        food: "Fruit Salad",
+        type: "Dessert",
+        movie_friendly: false,
+        rating_out_of_5: 0.5
+    },
+    {
+        food: "Tiramisu",
+        type: "Dessert",
+        movie_friendly: "false",
+        rating_out_of_5: 4
+    },
+    {
+        food: "Gobstoppers",
+        type: "Sweets",
+        movie_friendly: true,
+        rating_out_of_5: 2,
+    },
+    {
+        food: "Broccoli",
+        type: "Vegetable",
+        movie_friendly: false,
+        rating_out_of_5: 5
+    },
+    {
+        food: "Roast Dinner",
+        type: "Meal",
+        movie_friendly: false,
+        rating_out_of_5: 5
+    },
+    {
+        food: "Pizza",
+        type: "Junk",
+        movie_friendly: true, 
+        rating_out_of_5: 5
+    },
+    {
+        food: "Carrot",
+        type: "Vegetable",
+        movie_friendly: false,
+        rating_out_of_5: 4
+    },
+    {
+        food: "Chocolate",
+        type: "Junk",
+        movie_friendly: true,
+        rating_out_of_5: 4.5
+    },
+    {
+        food: "Salad",
+        type: "Meal",
+        movie_friendly: false,
+        rating_out_of_5: 2
+    },
+    {
+        food: "Ham Sandwich",
+        type: "Meal",
+        movie_friendly: true,
+        rating_out_of_5: 1.5,
+    },
+    {
+        food: "Cheese Sandwich",
+        type: "Meal",
+        movie_friendly: true, 
+        rating_out_of_5: 1
+    },
+    {
+        food: "Pickled Onions",
+        type: "Vegetable",
+        movie_friendly: false,
+        rating_out_of_5: 2
+    },
+    {
+        food: "Sausage Roll",
+        type: "Junk",
+        movie_friendly: true,
+        rating_out_of_5: 3.5
+    },
+    {
+        food: "Scotch Egg",
+        type: "Junk",
+        movie_friendly: true,
+        rating_out_of_5: 2.5
+    },
+    {
+        food: "Brussel Sprouts",
+        type: "Vegetable",
+        movie_friendly: false,
+        rating_out_of_5: 0.5
+    },
+    {
+        food: "Apple",
+        type: "Fruit",
+        movie_friendly: false,
+        rating_out_of_5: 4
+    },
+    {
+        food: "Orange",
+        type: "Fruit",
+        movie_friendly: true,
+        rating_out_of_5: 4.5
+    },
+    {
+        food: "Grapes",
+        type: "Fruit",
+        movie_friendly: true,
+        rating_out_of_5: 5
+    },
+    {
+        food: "Coconut",
+        type: "Fruit",
+        movie_friendly: false,
+        rating_out_of_5: 2
+    },
+    {
+        food: "Mango",
+        type: "Fruit",
+        movie_friendly: false,
+        rating_out_of_5: 5
+    },
+    {
+        food: "Passionfruit",
+        type: "Fruit",
+        movie_friendly: false,
+        rating_out_of_5: 3.5
+    },
+    {
+        food: "Calzone",
+        type: "Meal",
+        movie_friendly: false, 
+        rating_out_of_5: 4.5
+    },
+    {
+        food: "Skittles",
+        type: "Sweets",
+        movie_friendly: true, 
+        rating_out_of_5: 3.5
+    },
+    {
+        food: "Mars Bar",
+        type: "Junk",
+        movie_friendly: true,
+        rating_out_of_5: 4
+    },
+    {
+        food: "Pot Noodle",
+        type: "Meal",
+        movie_friendly: false, 
+        rating_out_of_5: 1.5
+    },
+    {
+        food: "Crackers",
+        type: "Junk",
+        movie_friendly: false,
+        rating_out_of_5: 1
+    },
+    {
+        food: "Celery",
+        type: "Vegetable",
+        movie_friendly: false,
+        rating_out_of_5: 0.5
+    },
+    {
+        food: "Fish & Chips",
+        type: "Meal",
+        movie_friendly: false,
+        rating_out_of_5: 4
+    },
+    {
+        food: "Jelly Beans",
+        type: "Sweets",
+        movie_friendly: true,
+        rating_out_of_5: 3
+    },
+    {
+        food: "Pork Pie",
+        type: "Junk",
+        movie_friendly: true,
+        rating_out_of_5: 0.5
+    },
+    {
+        food: "Banana",
+        type: "Fruit",
+        movie_friendly: true,
+        rating_out_of_5: 1
+    },
+    {
+        food: "Deep Fried Mars Bar",
+        type: "Junk",
+        movie_friendly: true,
+        rating_out_of_5: 1.5
+    },
+    {
+        food: "Leek",
+        type: "Vegetable",
+        movie_friendly: true,
+        rating_out_of_5: 0.5
+    }
+]
