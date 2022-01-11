7286dd45e6f9bf625b7c4bd34b2cf768521c949d
Edd Kleszcz <edd@edds-mbp.home>
2022-01-11 10:20:22 +0000

    Removing solutions from example files

--- challenges/challenge_2/buildTables/tableActors.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE actors(
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(32) NOT NULL,
-    last_name VARCHAR(32) NOT NULL,
-    dob DATE NOT NULL,
-    number_of_awards smallint,
-    number_of_nominations smallint
-);
--- challenges/challenge_2/buildTables/tablePeople.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE people (
-    id SERIAL PRIMARY KEY NOT NULL,
-    first_name VARCHAR(32) NOT NULL,
-    last_name VARCHAR(32) NOT NULL,
-    dob DATE NOT NULL,
-    favourite_film TEXT REFERENCES movies(title)
-);
